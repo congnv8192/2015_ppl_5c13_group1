9ecddd63d5508aeb1be3e8c67765bbc74499baec
slade77777 <dangxuancuong354@gmail.com>
2015-04-15 22:48:22 +0700

    dang xuan cuong

---
+++ cuongdx/TextProcessor.java
@@ -0,0 +1,81 @@
+package texttest;
+
+import java.util.ArrayList;
+
+//VPP : Verifiable procedural program.
+public class VPP {
+
+    public static void main(String[] args) {
+        
+       
+    }
+
+    
+    public static void UpdateData() {
+        //Read file data.
+
+        //Write this content after edited in data2.
+        
+        //processed each line
+    }
+
+    
+    /**
+     * This function use to count line and count word of each line and print result on standard screen.
+     * find every space to determine the number of word
+     * use the total character to determine the number of line
+     * @effect: Result the number of line and word each line.
+     * @return: number of line and word.
+     */
+    public static int CountLinesOriginalData() {
+        int count = 0;
+        //Read file txt.
+        
+        //Write result on standard screen.
+        
+        //process
+            return count;
+        }
+        //print result.
+        
+       
+    
+
+    
+    /**
+     * This function perform count word(not repeat) in data2.
+     * with every word save in array,find out the same word to refuse
+     * @effect: So luong tu khong tinh tu lap lai trong data2.
+     */
+    public static void CountDistinctWordsUpdatedData() {
+        //Read file data2
+        
+        //Write result in standard screen.
+        
+        //process.
+
+        
+            }
+        
+        //print result.
+    
+
+    
+    /**
+     * Read file data, remove redundant spaces, 
+     * uppercase all character
+     * of each letter,lowercase 2nd to last character by substring
+     * write this content in data2.
+     *@effect: this content of data2 completed. 
+     */
+    
+    public static void CountRedundantSpaces() {
+        //Read file data.
+
+        //Write result in standard screen.
+        
+        //process.
+       
+        //print result.
+    }
+}
