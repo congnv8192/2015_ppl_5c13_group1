48aba8df6bcfbdc187416c58fa065505bf4bba4b
ngotiendung2611 <ngotiendung2611@gmail.com>
2015-04-15 23:24:15 +0700

    Create TextProcessor.java

---
+++ Dungnt/TextProcessor.java
@@ -0,0 +1,78 @@
+package TextProcessor;
+
+import java.util.ArrayList;
+
+//VPP : Verifiable procedural program.
+public class VPP {
+
+    public static void main(String[] args) {
+        
+       
+    }
+
+    /**
+     * Read file data, remove redundant spaces, normalize words, write this content in data2.
+     *@effect: this content of data2 completed. 
+     */
+    public static void UpdateData() {
+        //Read file data.
+
+        //Write this content after edited in data2.
+        
+        //processed each line
+    }
+
+    
+    /**
+     * This function use to count line and count word of each line and print result on standard screen.
+     * @effect: Result the number of line and word each line.
+     * @return: number of line and word.
+     */
+    public static int CountLinesOriginalData() {
+        int count = 0;
+        //Read file txt.
+        
+        //Write result on standard screen.
+        
+        //process
+            return count;
+        }
+        //print result.
+        
+       
+    
+
+    
+    /**
+     * This function perform count word(not repeat) in data2.
+     * @effect: So luong tu khong tinh tu lap lai trong data2.
+     */
+    public static void CountDistinctWordsUpdatedData() {
+        //Read file data2
+        
+        //Write result in standard screen.
+        
+        //process.
+
+        
+            }
+        
+        //print result.
+    
+
+    
+    /**
+     * This function perform count redundant spaces.
+     * @effect: the number of spaces.
+     */
+    
+    public static void CountRedundantSpaces() {
+        //Read file data.
+
+        //Write result in standard screen.
+        
+        //process.
+       
+        //print result.
+    }
+}
