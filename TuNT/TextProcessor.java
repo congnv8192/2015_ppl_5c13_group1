e517139c6c7a21f044e6483713049e4274bb478f
loanhtu <loanhtu@gmail.com>
2015-04-15 21:41:40 +0700

    Create TextProcessor.java

---
+++ TuNT/TextProcessor.java
@@ -0,0 +1,64 @@
+public static void UpdateData() {
+     /**
+     * update content file by removing redundant spaces and normalizing words
+     *
+     * @requires : file path is valid
+     * @modifies 
+     * @effects : 
+     *  read file content into String content
+     *  remove redundant spaces in content
+     *  for each word in content
+     *      capitalize first letter
+     *      lowercase remaining letter
+     *  returns content
+     */
+    }
+
+    
+    
+    public static int CountLinesOriginalData() {
+        /**
+         * count number of lines of original data
+         */
+        
+        }
+        //print result.
+        
+       
+    
+
+    
+    /**
+     * this function perform count word(not repeat) in data2.
+     * 
+     * 
+     */
+    public static void CountDistinctWordsUpdatedData() {
+        //Read file data2
+        
+        //Write result in standard screen.
+        
+        //process.
+
+        
+            }
+        }
+        //print result.
+    
+
+    
+    /**
+     * this function perform count redundant spaces.
+     * 
+     */
+    
+    public static void CountRedundantSpaces() {
+        //Read file data.
+
+        //Write result in standard screen.
+        
+        //process.
+       
+        //print result.
+    }
+}
