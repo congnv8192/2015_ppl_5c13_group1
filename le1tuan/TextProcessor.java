5cae9b901499e0095513b031d99f77e9c7d43c49
le1tuan <lekoi9x@gmail.com>
2015-04-16 20:37:40 +0700

    Lê Tuấn Anh

---
+++ le1tuan/TextProcessor.java
@@ -0,0 +1,64 @@
+/**
+     * update content file by removing redundant spaces and normalizing words
+     *
+     * @requires : file path is valid
+     * @modifies 
+     * @effects : 
+     *  read file content into String content
+     *  remove redundant spaces in content
+     *  for each word in content
+     *      capitalize first letter
+     *      lowercase remaining letter
+     *  returns content
+     */
+    
+     public static String task2(String filepath) {
+    }
+    
+    
+    
+    /**
+     * Count number of lines in original content and distinct words in the update content
+     * 
+     * @requires: file path is valid
+     * @modifies
+     * @effects:
+     * read file content into String content
+     * count the number of lines in content
+     * read the update content
+     * count the number of distinct words in the update content in task2
+     * returns content
+     */
+     public static int task3(String filepath) {
+    }
+    
+    
+    
+    /**
+     * write the updated content in task2 to file named "data2.txt"
+     * @requires: file path is valid
+     * @modifies: file path
+     * @effects:
+     * read file update content 
+     * write it to another text file named "data2.txt"
+     */
+     public static String task4(String filepath) {
+        
+    }
+    
+    
+    
+    /**
+     * display a brief text-based report summarising the results of tasks 2, 3 and 4. 
+     * @requires: file path is valid
+     * @modifies:
+     * @effects:
+     * display number of original lines 
+     * display number of original words
+     * display number of redundant spaces removed
+     * display number of distinct words
+     * display data2.txt is updated file from data.txt
+     */
+    public static String task5(String filepath) {
+        
+    }
